09c32c4deffae5334e84d0e36ea9a12bc8d2d226
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-16 15:02:32 +0100

    Push milestone 0

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const myTeam = [
+    {
+        name: 'Wayn Barnett',
+        role:'Founder & CEO',
+        myImage: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Carol',
+        role: 'Chief Editor',
+        myImage: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        myImage: 'walter-gordon-office-manager.jpg'
+    }, {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        myImage: 'angela-lopez-social-media-manager.jpg'
+    }, {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        myImage: 'scott-estrada-developer.jpg'
+    }, {
+        name: 'Barbara Ramos',
+        role: 'Graphic Desginer',
+        myImage: 'barbara-ramos-graphic-designer.jpg'
+    }    
+];
