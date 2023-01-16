f1e32f09b5420831ae16d3985772a1faa41671a9
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-16 15:19:15 +0100

    Update MILESTONE 1

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 const myTeam = [
     {
-        name: 'Wayn Barnett',
-        role:'Founder & CEO',
+        myName: 'Wayn Barnett',
+        myRole:'Founder & CEO',
         myImage: 'wayne-barnett-founder-ceo.jpg'
     },
     {
-        name: 'Angela Carol',
-        role: 'Chief Editor',
+        myName: 'Angela Carol',
+        myRole: 'Chief Editor',
         myImage: 'angela-caroll-chief-editor.jpg'
     },
     {
-        name: 'Walter Gordon',
-        role: 'Office Manager',
+        myName: 'Walter Gordon',
+        myRole: 'Office Manager',
         myImage: 'walter-gordon-office-manager.jpg'
     }, {
-        name: 'Angela Lopez',
-        role: 'Social Media Manager',
+        myName: 'Angela Lopez',
+        myRole: 'Social Media Manager',
         myImage: 'angela-lopez-social-media-manager.jpg'
     }, {
-        name: 'Scott Estrada',
-        role: 'Developer',
+        myName: 'Scott Estrada',
+        myRole: 'Developer',
         myImage: 'scott-estrada-developer.jpg'
     }, {
-        name: 'Barbara Ramos',
-        role: 'Graphic Desginer',
+        myName: 'Barbara Ramos',
+        myRole: 'Graphic Desginer',
         myImage: 'barbara-ramos-graphic-designer.jpg'
     }    
 ];
@@ -31,7 +31,7 @@ const myTeam = [
 for (let index = 0; index < myTeam.length; index++) {
     // const element = myTeam[index];
     for (let key in myTeam[index]) {
-        console.log(myTeam[index][key]);
+        console.log(key + ': ' + myTeam[index][key]);
     }
     console.log('--------------');
 }
